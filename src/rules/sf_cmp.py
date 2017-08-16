347f126dc2712168a3067be5e17d09cac55cf559
evansj <evansj@email.chop.edu>
2017-08-16 10:58:47 -0400

    are domains enriched in pathogenic vars?

--- src/rules/sf_cmp.py
+++ src/rules/sf_cmp.py
@@ -1,4 +1,4 @@
-"""Fisher tests"""
+"""Fisher tests for burdened domains"""
 import pandas
 import statsmodels.sandbox.stats.multicomp as fdr
 from const import *
@@ -25,6 +25,19 @@ rule fdr:
         (reject_ls, qs, alpha_sidak, alpha_bond) = fdr.multipletests(df['pval'].values, method='fdr_bh')
         df['qval'] = qs
         df.sort_values(by='qval', ascending=True).to_csv(output.o, index=False, sep='\t')
+
+rule flag_sig_domains:
+    input:  d = DATA + 'interim/EPIv6.eff.dbnsfp.anno.hHack.splitPfam.dat',
+            q = DATA + 'interim/enrich_q/missense_variant.xls'
+    output: o = DATA + 'interim/EPIv6.eff.dbnsfp.anno.hHack.splitPfam.missense.dat'
+    run:
+        use_cols = ['pfam', 'fg_gtr', 'qval']
+        r = {'fg_gtr':'missense_fg_gtr',
+             'qval':'missense_qval'}
+        q_df = pandas.read_csv(input.q, sep='\t', usecols=use_cols).rename(columns=r)
+        df = pandas.read_csv(input.d, sep='\t')
+        m = pandas.merge(df, q_df, on='pfam', how='left')
+        m.to_csv(output.o, index=False, sep='\t')
 
 rule m:
     input: expand(DATA + 'interim/enrich_q/{var}.xls', var=('missense_variant', 'stop_gained'))
