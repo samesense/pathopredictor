34593741b5a80f081344cf24f5dc40cd3238c0e0
evansj <evansj@email.chop.edu>
2017-11-08 14:33:53 -0500

    mk denovo vcf

--- src/rules/sf_denovo.py
+++ src/rules/sf_denovo.py
@@ -5,3 +5,41 @@ rule mk_denovo_vcf:
             DATA + 'raw/EPIv6.xlsx'
     output: DATA + 'interim/denovo/denovo.vcf'
     shell:  '  python {SCRIPTS}limit_denovo_genes.py {input} {output}'
+
+DBNSFP_FIELDS = 'Interpro_domain,SIFT_score,Polyphen2_HVAR_pred,RadialSVM_pred,LR_pred,Reliability_index,FATHMM_pred,MutationAssessor_pred,MutationTaster_pred,phyloP100way_vertebrate,phastCons100way_vertebrate'
+
+rule snpeff_denovo:
+    input:  DATA + 'interim/denovo/denovo.vcf' 
+    output: DATA + 'interim/denovo/denovo.eff.vcf'
+    shell:  """{JAVA} -Xmx32g -Xms16g -jar {EFF} eff \
+               -strict -noStats hg19 -c {EFF_CONFIG} \
+               {input} > {output}"""
+
+
+rule annotateDbnsfp_denovo:
+    input:  DATA + 'interim/denovo/denovo.eff.vcf'
+    output: DATA + 'interim/denovo/denovo.use.eff.dbnsfp.vcf'
+    shell:  """{JAVA} -Xmx32g -Xms16g -jar {SIFT} dbnsfp -v \
+               -db {SIFT_DBNSFP} -f {DBNSFP_FIELDS} {input} > {output}"""
+
+# fix pfam
+# /mnt/isilon/cbmi/variome/bin/gemini/data/gemini_data/hg19.pfam.ucscgenes.enum.bed.gz
+# ann fixed pfam
+# parse genes
+rule vcfanno_denovo:
+    input:   vcf = DATA + 'interim/denovo/denovo.use.eff.dbnsfp.vcf',
+             conf = CONFIG + 'vcfanno.conf',
+             lua = VCFANNO_LUA_FILE
+    output:  DATA + 'interim/denovo/denovo.anno.vcf'
+    threads: 10
+    shell:   """{VCFANNO} -p {threads} -base-path {GEMINI_ANNO} -lua {input.lua} \
+                {input.conf} {input.vcf} > {output}"""
+
+HEADER_FIX = 'eff_indel_splice,1,Flag AC,1,Integer AF,1,Float dbNSFP_FATHMM_pred,.,String dbNSFP_Interpro_domain,.,String dbNSFP_LR_pred,.,String dbNSFP_phyloP100way_vertebrate,.,String dbNSFP_phastCons100way_vertebrate,.,String dbNSFP_SIFT_score,.,String dbNSFP_Reliability_index,.,String dbNSFP_RadialSVM_pred,.,String dbNSFP_RadialSVM_pred,.,String dbNSFP_Polyphen2_HVAR_pred,.,String dbNSFP_MutationTaster_pred,.,String dbNSFP_MutationAssessor_pred,.,String'
+
+rule fixHeader_denovo:
+    input:  DATA + 'interim/denovo/denovo.anno.vcf'
+    output: DATA + 'interim/denovo/denovo.anno.hHack.vcf'
+    shell:  'python {HEADER_HCKR} {input} {output} {HEADER_FIX}'
+
+
--- src/scripts/limit_denovo_genes.py
+++ src/scripts/limit_denovo_genes.py
@@ -6,7 +6,7 @@ dat_file, xls_file, out_file = sys.argv[1:]
 df_pre = pandas.read_excel(xls_file)
 genes = set(df_pre['Gene Symbol'].values)
 cols = ['Chr', 'Position', 'Variant']
-denovo_df = pandas.read_csv(dat_file, skiprows=0, header=1, sep='\t')
+denovo_df = pandas.read_csv(dat_file, dtype={'Position':int}, skiprows=0, header=1, sep='\t')
 #print(denovo_df.head())
 def filter_genes(row):
     return row['Gene'] in genes
@@ -14,8 +14,10 @@ new_data =[]
 
 for row in denovo_df[denovo_df.apply(filter_genes, axis=1)][cols].itertuples():
     ref, alt = row.Variant.split('>')
-    ls = (str(row.Chr), str(row.Position), '', ref, alt, '.', '.')
+    ls = (row.Chr, row.Position, '', ref, alt, '.', '.')
     new_data.append(ls)
 idx = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER']
 new_df = pandas.DataFrame(new_data, columns=idx)
-new_df.drop_duplicates().sort_values(by=['#CHROM', 'POS']).to_csv(out_file, index=False, sep='\t')
+with open(out_file, 'w') as fout:
+    print('##fileformat=VCFv4.0', file=fout)
+    new_df.drop_duplicates().sort_values(by=['#CHROM', 'POS'], ascending=[True, True]).to_csv(fout, index=False, sep='\t')
