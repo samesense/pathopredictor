4508a55128121d72d4b36cb5101e875f44bcb580
evansj <evansj@email.chop.edu>
2017-11-08 11:30:50 -0500

    add old files

--- src/rules/sf_mutalyzer.py
+++ src/rules/sf_mutalyzer.py
@@ -11,7 +11,19 @@ rule lab_two:
     output: DATA + 'interim/mutalyzer_input/panel_two'
     shell:  'python {SCRIPTS}mk_dat_panel_two.py {input} {output}'
 
+# uc    
 rule lab_three:
     input:  DATA + 'raw/UC_all_panel_variants_01_20_2016.xlsx'
     output: DATA + 'interim/mutalyzer_input/panel_uc'
     shell:  'python {SCRIPTS}mk_dat_panel_uc.py {input} {output}'
+
+# rule fix_mutalyzer_wtf:
+#     input:  DATA + 'raw/mutalyzer.uc'
+#     output: DATA + 'raw/mutalyzer.uc.fix'
+#     shell:  'cut -f 1,3 {input} > {output}'
+
+rule fix_mutalyzer:
+    input:  DATA + 'raw/mutalyzer.{panel}'
+    output: DATA + 'raw/mutalyzer.{panel}.fix'
+    shell:  'cut -f 1,3 {input} > {output}'
+    
