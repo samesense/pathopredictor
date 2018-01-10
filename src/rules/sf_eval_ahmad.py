e884a07778baf963e86b1dd44ae406acda6e2f7f
evansj <evansj@email.chop.edu>
2018-01-10 09:10:51 -0500

    ahmda plots group by disease

--- src/rules/sf_eval_ahmad.py
+++ src/rules/sf_eval_ahmad.py
@@ -11,11 +11,10 @@ rule percent_wrong:
     output: o = WORK + '{method}.{cols}.eval_panel.eval.percentWrong'
     run:
         def calc_wrong_percent(rows):
-            print( len(rows) )
             tot_wrong = list(rows[rows.eval_type == 'TotWrong']['var_count'].values)[0]
             tot_preds = sum(rows[rows.eval_type != 'TotWrong']['var_count'])
-            print('tw', tot_wrong)
-            print('s', tot_preds)
+            # print('tw', tot_wrong)
+            # print('s', tot_preds)
             return tot_wrong/tot_preds
 
         def load_other_df(afile):
@@ -24,17 +23,18 @@ rule percent_wrong:
                         .apply(calc_wrong_percent)
                         .reset_index()
                         .rename(columns={0:'percent_wrong', 'score_type':'score_type_pre'}) )
-            panel_df['score_type'] = panel_df['clinvar_type'] + '::' + panel_df['score_type_pre']
-#            print(panel_df.head())
+            panel_df['st'] = panel_df.apply(lambda row: row['score_type_pre'].split(row['disease'])[0], axis=1)
+            panel_df['score_type'] = panel_df['clinvar_type'] + '::' + panel_df['st']
             return panel_df[['disease', 'percent_wrong', 'score_type']]
 
         non_panel_dfs = pd.concat( [load_other_df(afile) for afile in input.non_panel] )
+        non_panel_dfs['color'] = 'predict clinvar'
         panel_df = (pd.read_csv(input.panel, sep='\t')
                     .groupby(('disease', 'score_type'))
                     .apply(calc_wrong_percent)
                     .reset_index()
-                    .rename(columns={0:'percent_wrong'})
-                    .to_csv(output.o, index=False, sep='\t') )
+                    .rename(columns={0:'percent_wrong'}) )
+        panel_df['color'] = 'predict_panel'
         pd.concat([panel_df, non_panel_dfs]).to_csv(output.o, index=False, sep='\t')
 
 rule plot_ahmad:
@@ -45,9 +45,9 @@ rule plot_ahmad:
           require(ggplot2)
           d = read.delim("{input}", sep='\t', header=TRUE)
           p = ggplot(data=d) +
-          geom_col(aes(y=percent_wrong,x=score_type, fill=score_type)) +
+          geom_col(aes(y=percent_wrong,x=score_type, fill=color)) +
           facet_grid(disease~., scale='free') + theme_bw() +
-          ylab('Wrong Predictions') +
+          ylab('Wrong prediction fraction') +
           theme(axis.text.x = element_text(angle=45, hjust=1)) +
           xlab('') + theme(legend.position="none")
           ggsave("{output}", p)
