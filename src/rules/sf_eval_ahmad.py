6cddcfa0dee24fa83b403966c522455400502ebe
evansj <evansj@email.chop.edu>
2018-01-10 09:49:56 -0500

    clean up plots by disease

--- src/rules/sf_eval_ahmad.py
+++ src/rules/sf_eval_ahmad.py
@@ -27,6 +27,17 @@ rule percent_wrong:
             panel_df['score_type'] = panel_df['clinvar_type'] + '::' + panel_df['st']
             return panel_df[['disease', 'percent_wrong', 'score_type']]
 
+        def rename_score(score_type):
+            if score_type == 'global_PredictionStatusMPC>2':
+                return 'paper_' + wildcards.cols
+            elif '::' in score_type:
+                return 'predict_' + score_type.split('::')[0]
+            elif score_type == 'global_PredictionStatusMPC':
+                return 'panel-trained'
+            elif 'global' == score_type[:6]:
+                return score_type.replace('global_PredictionStatusMPC', 'train_with')
+            return score_type            
+                
         non_panel_dfs = pd.concat( [load_other_df(afile) for afile in input.non_panel] )
         non_panel_dfs['color'] = 'predict clinvar'
         panel_df = (pd.read_csv(input.panel, sep='\t')
@@ -35,7 +46,13 @@ rule percent_wrong:
                     .reset_index()
                     .rename(columns={0:'percent_wrong'}) )
         panel_df['color'] = 'predict_panel'
-        pd.concat([panel_df, non_panel_dfs]).to_csv(output.o, index=False, sep='\t')
+        m = pd.concat([panel_df, non_panel_dfs])
+        m.loc[:, 'st'] = m.apply(lambda row: rename_score(row['score_type']), axis=1)
+        crit = m.apply(lambda row: not row['disease'] in ('ALL', 'no_disease')
+                       and not 'earing' in row['disease']
+                       and not 'issue' in row['disease'],
+                       axis=1)
+        m[crit].to_csv(output.o, index=False, sep='\t')
 
 rule plot_ahmad:
     input:  WORK + '{method}.{cols}.eval_panel.eval.percentWrong'
@@ -45,12 +62,12 @@ rule plot_ahmad:
           require(ggplot2)
           d = read.delim("{input}", sep='\t', header=TRUE)
           p = ggplot(data=d) +
-          geom_col(aes(y=percent_wrong,x=score_type, fill=color)) +
-          facet_grid(disease~., scale='free') + theme_bw() +
+          geom_col(aes(y=percent_wrong, x=st, fill=color)) +
+          facet_grid(disease~.) + theme_bw() +
+          theme(axis.text.x = element_text(angle=90, hjust=1)) +
           ylab('Wrong prediction fraction') +
-          theme(axis.text.x = element_text(angle=45, hjust=1)) +
-          xlab('') + theme(legend.position="none")
-          ggsave("{output}", p)
+          xlab('') + theme(legend.position="none") + coord_flip()
+          ggsave("{output}", p, height=10)
           """)
 
 rule all_ahmad:
