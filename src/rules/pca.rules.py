c66dab9a0615e993ebee1335cd31ab7a120d31d1
evansj <evansj@email.chop.edu>
2018-02-07 09:29:46 -0500

    Explore epi and clinvar pca.

---
+++ src/rules/pca.rules.py
@@ -0,0 +1,9 @@
+"""Plot pca by gene to see panel vs clinvar"""
+import pandas as pd
+from functools import reduce
+from itertools import combinations, chain
+
+include: "const.py"
+
+from snakemake.utils import R
+
