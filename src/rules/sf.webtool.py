e1fe8dcb3fc76c8da99fdeb11924eb707ad9fd82
evansj <evansj@email.chop.edu>
2018-02-15 12:01:38 -0500

    Dump genes for estimate of website data rows.

--- src/rules/sf.webtool.py
+++ src/rules/sf.webtool.py
@@ -4,3 +4,15 @@ rule mk_sample_data:
     run:
         keys = ['Disease', 'gene', 'chrom', 'pos', 'ref', 'alt', 'Protein_Change', 'y', 'mpc_pred'] + list(feats)
         pd.read_csv(input.i, sep='\t')[keys].rename(columns={'y':'obs_class','mpc_pred':'predicted_class'}).to_csv(output.o, index=False, sep=',')
+
+rule mk_genes:
+    input:  DATA +  'interim/other/other.eff.dbnsfp.anno.hHack.dat.limit.xls', 
+            expand(DATA + 'interim/epi/{lab}.eff.dbnsfp.anno.hHack.dat.limit.xls', lab=('uc', 'EPIv6') )
+    output: o=WORK + 'genes'
+    run:
+        df = pd.concat([pd.read_csv(afile, sep='\t')
+                        for afile in list(input)])
+        genes = set(df['gene'])
+        with open(output.o, 'w') as fout:
+            for gene in genes:
+                print(gene, file=fout)
