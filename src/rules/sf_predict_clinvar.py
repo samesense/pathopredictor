ac3004b2a64d59b81690eaab802a73cf9ed77086
evansj <evansj@email.chop.edu>
2018-03-02 08:24:58 -0500

    Combine clinvar plot diseases #30 #31.

--- src/rules/sf_predict_clinvar.py
+++ src/rules/sf_predict_clinvar.py
@@ -6,7 +6,7 @@ rule eval_by_gene_clinvar:
     input:  i = WORK + 'roc_df_{eval_source}/{features}'
     output: o = DATA + 'interim/pred_clinvar_eval/{eval_source}.{features}'
     run:
-        keys = ['Disease', 'y']
+        keys = ['Disease']
         df_pre = pd.read_csv(input.i, sep='\t')
         crit = df_pre.apply(lambda row: not 'uc' in row['Disease'] and not 'issue' in row['Disease'] and not 'earing' in row['Disease'], axis=1)
         df = df_pre[crit]
@@ -14,16 +14,46 @@ rule eval_by_gene_clinvar:
         tot_wrong_df = df.groupby(['Disease']).apply(calc_wrong).reset_index().rename(columns={0:'predictorWrongFracTot'})
         size_df = df.groupby(keys).size().reset_index().rename(columns={0:'size'})
         m = pd.merge(wrong_df, size_df, on=keys)
-        m.loc[:, 'var_class'] = m.apply(lambda row: 'pathogenic' if row['y'] == 1 else 'benign', axis=1)
         m.loc[:, 'clinvar_type'] = m.apply(lambda row: row['Disease'].split(':')[1], axis=1)
         m.loc[:, 'dd'] = m.apply(lambda row: row['Disease'].split(':')[0], axis=1)
-        pd.merge(m, tot_wrong_df, on='Disease', how='left')[['Disease', 'dd', 'var_class', 'clinvar_type', 'size', 'predictorWrongFracTot', 'wrongFrac']].to_csv(output.o, index=False, sep='\t')
+        pd.merge(m, tot_wrong_df, on='Disease', how='left')[['Disease', 'dd', 'clinvar_type', 'size', 'predictorWrongFracTot', 'wrongFrac']].to_csv(output.o, index=False, sep='\t')
 
 rule combine_features_by_gene_clinvar_plot:
     input:  expand(DATA + 'interim/pred_clinvar_eval/{{eval_source}}.{feature}', feature=COMBO_FEATS)
     output: o=DATA + 'interim/{eval_source}.by_gene_feat_combo.predictFullClinvar'
     run:
-        pd.concat([read_gene_df(afile) for afile in list(input)]).to_csv(output.o, index=False, sep='\t')
+        disease_order = {'genedx-epi-limitGene':3,
+                         'Rasopathies':4,
+                         'genedx-epi':2,
+                         'Cardiomyopathy':1}
+
+        diseases = {'genedx-epi-limitGene':'Epilepsy (dominant genes)',
+                    'Rasopathies':'Rasopathies',
+                    'genedx-epi':'Epilepsy',
+                    'Cardiomyopathy':'Cardiomyopathy'}
+        clinvar_names  = {'tot': 'Total ClinVar',
+                          'single': 'ClinVar w/ Evidence'}
+        dfp = pd.concat([read_gene_df(afile) for afile in list(input)])
+        crit = dfp.apply(lambda row: row['clinvar_type'] in clinvar_names, axis=1)
+        df = dfp[crit]
+        df.loc[:, 'dd'] = df.apply(lambda row: diseases[row['dd']], axis=1)
+        df.loc[:, 'clinvar_type'] = df.apply(lambda row: clinvar_names[row['clinvar_type']], axis=1)
+        df.to_csv(output.o, index=False, sep='\t')
+
+rule plot_clinvar_eval_paper:
+    input:  DATA + 'interim/clinvar.by_gene_feat_combo.predictFullClinvar'
+    output: DOCS + 'paper_plts/fig4_eval_clinvar.pdf'
+    run:
+        plot_cmd = 'geom_col(aes(y=wrongFrac, x=reorder(combo, predictorWrongFracTot)), position="dodge")'
+        R("""
+          require(ggplot2)
+          d = read.delim("{input}", sep='\t', header=TRUE)
+          p = ggplot(data=d) + {plot_cmd} +
+              ylab('Incorrect prediction fraction') + xlab('') + theme_bw() + facet_grid(clinvar_type~dd) +
+              labs(fill="Variant class") + coord_flip() +
+              theme(axis.text.y = element_text(size=10), legend.position="bottom")
+          ggsave("{output}", p, height=10, width=20)
+          """)
 
 rule plot_clinvar_eval:
     input:  DATA + 'interim/{eval_source}.by_gene_feat_combo.predictFullClinvar'
