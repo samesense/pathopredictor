767cbdc3aceb0fb4ec477f7aa00edf2010fe854a
evansj <evansj@email.chop.edu>
2018-05-17 10:30:09 -0400

    Count max patients used for a gene for LMM diseases.

--- src/rules/sf_patient_counts.py
+++ src/rules/sf_patient_counts.py
@@ -9,3 +9,14 @@ rule epi_counts:
         max_count = max(df['final_count'])
         with open(output.o, 'w') as fout:
             print('Epilesy\t' + str(max_count), file=fout)
+
+rule other_panel_counts:
+    input:  i = DATA + 'raw/Clinicalvariants_LMM.xlsx',
+            gene_info = DATA + 'raw/gene_disease.xlsx'
+    output: o = DATA + 'interim/patient_counts/other'
+    run:
+        df = pd.read_excel(input.i).rename(columns={'Gene Symbol':'gene'})
+        disease_df = pd.read_excel(input.gene_info, skiprows=[0,1,2]).rename(columns={'Gene':'gene'})
+        df = pd.merge(disease_df, df, on='gene', how='left')
+        df.groupby('Disease')[['# of probands']].max().reset_index().to_csv(output.o, index=False, sep='\t')
+        
