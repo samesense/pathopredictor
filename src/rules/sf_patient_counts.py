5cb280dd023f5f982a335a4c45b63b1dd1c4eba0
evansj <evansj@email.chop.edu>
2018-05-29 07:27:43 -0400

    Dump all panel genes #77.

--- src/rules/sf_patient_counts.py
+++ src/rules/sf_patient_counts.py
@@ -2,7 +2,8 @@
 
 rule epi_counts:
     input:  i = DATA + 'raw/EPIv6.xlsx'
-    output: o = DATA + 'interim/patient_counts/epi'
+    output: o = DATA + 'interim/patient_counts/epi',
+            genes = DATA + 'interim/panel_genes/epi'
     run:
         def calc_epi_class(row):
             if 'BENIGN' in row['Classification']:
@@ -13,13 +14,17 @@ rule epi_counts:
                 return 'VUS'
 
         df = pandas.read_excel(input.i)
-        print(df.columns.values)
+        gene_ls = set([x for x in df['Gene Symbol'] if x.strip()])
         genes = len( set([x for x in df['Gene Symbol'] if x.strip()]) )
+        with open(output.genes, 'w') as fout:
+            print('Disease\tgene', file=fout)
+            for gene in gene_ls:
+                print('EPI\t' + gene, file=fout)
+
         df.loc[:, 'final_count'] = df.apply(lambda row: row['Pos Fam Cnt'] + row['Neg Fam Cnt'], axis=1)
         df.loc[:, 'c'] = df.apply(calc_epi_class, axis=1)
         max_count = max(df['final_count'])
         g = df[['c']].groupby('c').size().reset_index()
-        print(g)
         benign = g[g.c=='Benign'][0].values[0]
         path = g[g.c=='Pathogenic'][0].values[0]
         vus = g[g.c=='VUS'][0].values[0]
@@ -31,20 +36,21 @@ rule epi_counts:
 rule other_panel_counts:
     input:  i = DATA + 'raw/Clinicalvariants_LMM.xlsx',
             gene_info = DATA + 'raw/gene_disease.xlsx'
-    output: o = DATA + 'interim/patient_counts/other'
+    output: o = DATA + 'interim/patient_counts/other',
+            genes = DATA + 'interim/panel_genes/other'
     run:
         def calc_epi_class(row):
             if 'enign' in str(row['Cat (Dis)']):
                 return 'Benign'
             elif 'ath' in str(row['Cat (Dis)']):
                 return 'Pathogenic'
-            elif 'Un' in str(row['Cat (Dis)']) or 'Cat' in str(row['Cat (Dis)']):
+            elif 'nan' == str(row['Cat (Dis)']) or 'Un' in str(row['Cat (Dis)']) or 'Cat' in str(row['Cat (Dis)']):
                 return 'VUS'
             else:
                 print(row['Cat (Dis)'])
+                i = 1/0
 
         def eval_disease(df, disease):
-            print(df.columns)
             genes = len( set([x for x in df['gene'] if x.strip()]) )
             df.loc[:, 'c'] = df.apply(calc_epi_class, axis=1)
             g = df[['c']].groupby('c').size().reset_index()
@@ -57,12 +63,20 @@ rule other_panel_counts:
         df = pd.read_excel(input.i).rename(columns={'Gene Symbol':'gene'})
         disease_df = pd.read_excel(input.gene_info, skiprows=[0,1,2]).rename(columns={'Gene':'gene'})
         df = pd.merge(disease_df, df, on='gene', how='left')
+        crit = df.apply(lambda row: row['gene'].strip() and not 'ear' in row['Disease'] and not 'onnec' in row['Disease'], axis=1)
+        df[crit][['Disease', 'gene']].drop_duplicates().to_csv(output.genes, index=False, sep='\t')
         pat_count_df = df.groupby('Disease')[['# of probands']].max().reset_index().rename(columns={'# of probands':'Max Patient Count'})
         ls = []
         for disease in set(df['Disease']):
             ls.append(eval_disease(df[df.Disease==disease], disease))
         df = pd.merge(pd.concat(ls), pat_count_df, on='Disease', how='left')
         df.to_csv(output.o, index=False, sep='\t')
+
+rule concat_patient_genes:
+    input: expand(DATA + 'interim/panel_genes/{d}', d=('other', 'epi'))
+    output: o = DATA + 'interim/panel_genes/panel.tab'
+    run:
+        pd.concat([pd.read_csv(afile, sep='\t') for afile in input]).to_csv(output.o, index=False, sep='\t')
 
 rule concat_patient_counts:
     input: expand(DATA + 'interim/patient_counts/{d}', d=('other', 'epi'))
