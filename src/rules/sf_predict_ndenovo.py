d03333a3652559274d01cfcb112ba098b1a80d98
evansj <evansj@email.chop.edu>
2018-10-12 14:56:53 -0400

    Cleanup ndenovo fig.

--- src/rules/sf_predict_ndenovo.py
+++ src/rules/sf_predict_ndenovo.py
@@ -2,21 +2,32 @@ rule plot_ndenovo_eval_paper:
     input:  i = DATA + 'interim/EVAL_ndenovo/pred_clinvar_eval/clinvar_tot.' + C_FEATS
     output: o = DOCS + 'paper_plts/fig8c_evalDenovoAvgPr.tiff'
     run:
-        plot_cmd = """geom_col( aes(y=avg_pr, x=reorder(features, avg_pr)) )"""
+        plot_cmd = """geom_col( aes(y=avg_pr, x=reorder(new_feat_name, avg_pr)) ) +
+                      geom_text(size=2, hjust="left", colour="white", data=label_df, aes(x=x, y=y, label=label))"""
 
         df_tot = pd.read_csv(input.i, sep='\t')
         crit = df_tot.apply(lambda row: 'Epi' in row['disease_name'] and ('MPC'==row['features'] or 'REVEL'==row['features'] or 'PathoPredictor'==row['features']), axis=1)
-        # df_tot.loc[:, 'feature_color'] = df_tot.apply(lambda row: 'bomdo' if row['features']=='Combination' else 'feat', axis=1)
-        df_tot[crit].to_csv(output.o + '.df', index=False, sep='\t')
+        plot_df = df_tot[crit]
+        plot_df.loc[:, 'new_feat_name'] = plot_df.apply(lambda row: row['features'] if 'PathoPredictor' != row['features'] else row['features'] + '-' + row['disease_name'], axis=1)
+        crit = plot_df.apply(lambda row: 'Epilepsy' == row['disease_name'] or 'PathoPredictor'==row['features'], axis=1)
+        plot_df[crit].to_csv(output.o + '.df', index=False, sep='\t')
+
+        df = plot_df[['benign_size', 'pathogenic_size']].drop_duplicates().melt(var_name='var_type')
+        df.loc[:, 'label'] = df.apply(lambda row: row['var_type'].split('_')[0][0] + '=%d' % (row['value']), axis=1)
+        df.loc[:, 'x'] = df.apply(lambda row: 'MPC' if 'p' in row['label'] else 'REVEL', axis=1)
+        df['y'] = .01
+        df.to_csv(output.o + '.tmp.bar.labels', index=False, sep='\t')
 
         R("""
           require(ggplot2)
           require(grid)
           feature_palette <- c("#D4ED91", "grey")
           d = read.delim("{output}.df", sep='\t', header=TRUE)
+          label_df = read.delim("{output}.tmp.bar.labels", sep="\t", header=TRUE)
+
           p = ggplot(data=d) + {plot_cmd} + guides(fill=FALSE) +
-              ylab('Average precision') + xlab('') + theme_bw(base_size=12) + facet_grid(.~disease_name) +
-              coord_flip() + theme(axis.text.x = element_text(angle=90, vjust=.5, hjust=1, size=12)) 
+              ylab('Average precision') + xlab('') + theme_bw(base_size=12) +
+              coord_flip() + theme(axis.text.x = element_text(angle=90, vjust=.5, hjust=1, size=12))
           tiff("{output}", res=300, units="cm", height=3.5, width=10)
           grid.draw(p)
           grid.text("c", x=0.05, y=0.96)
@@ -29,11 +40,7 @@ rule plot_ndenovo_pr_curve_paper:
     run:
         plot_cmd = """geom_line( aes(y=Precision, x=Recall, colour=new_feat_name))"""
         df_tot = pd.read_csv(input.i, sep='\t')
-        #crit = df_tot.apply(lambda row: 'Epi' in row['disease_name'] and 'MPC'==row['features'], axis=1)
-        #crit = df_tot.apply(lambda row: 'Epi' in row['disease_name'] and 'PathoPredictor'==row['features'], axis=1)
         crit = df_tot.apply(lambda row: 'Epi' in row['disease_name'] and ('MPC' == row['features'] or 'REVEL'==row['features'] or 'PathoPredictor'==row['features']), axis=1)
-        #crit = df_tot.apply(lambda row: 'Epi' in row['disease_name'] and ('MPC' == row['features'] or 'PathoPredictor'==row['features']), axis=1)
-        #crit = df_tot.apply(lambda row: 'Epi' in row['disease_name'] and ('REVEL'==row['features'] or 'PathoPredictor'==row['features']), axis=1)
         plot_df = df_tot[crit]
         plot_df.loc[:, 'new_feat_name'] = plot_df.apply(lambda row: row['features'] if 'PathoPredictor' != row['features'] else row['features'] + '\n' + row['disease_name'], axis=1)
         crit = plot_df.apply(lambda row: 'Epilepsy' == row['disease_name'] or 'PathoPredictor'==row['features'], axis=1)
@@ -47,5 +54,8 @@ rule plot_ndenovo_pr_curve_paper:
               theme(axis.text.x = element_text(angle=90, vjust=.5, hjust=1, size=10)) +
               ylab('Precision') + labs(colour = "", fill="") +
               xlab('Recall')
-          ggsave("{output}", dpi=300, units="cm", height=3.5, width=10)
+          tiff("{output}", res=300, units="cm", height=3.5, width=10)
+          grid.draw(p)
+          grid.text("b", x=0.05, y=0.96)
+          dev.off()
           """)
