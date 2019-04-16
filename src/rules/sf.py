9f9cd8d27021901ebcad22e1a3f0f2c5d0d0818e
evansj <evansj@email.chop.edu>
2019-04-16 16:20:49 -0400

    Submitted.

--- src/rules/sf.py
+++ src/rules/sf.py
@@ -45,6 +45,7 @@ rule all_paper_plots:
 rule upload_all:
     input:
            expand(DBox.remote('ahmad_predictor/{fig}.png'), fig=FIGS),
+           expand(DBox.remote('ahmad_predictor/{fig}.tiff'), fig=FIGS),
            expand(DBox.remote('ahmad_predictor/{table}.csv'), table=TABLES)
 # rule s2:
 #     input: DATA + 'interim/man/man.eff.dbnsfp.anno.dat.xls',
